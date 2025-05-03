f3099944e321583f1db68b4e16ee33f383710b37
Fenyep <0Uiimi1D5/-yYkGt>
2025-05-03 10:14:01 +0100

    4 rows in the portfolio section

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -6,7 +6,8 @@ import { images1, images2, images3 } from "@/lib/data";
 
 export default function Portfolio() {
   return (
-    <div className="bg-white w-screen overflow-hidden max-h-screen pt-24 grid grid-cols-2 px-6 md:px-0 md:grid-cols-3 gap-y-3.5 gap-x-4 md:gap-x-8 md:gap-y-8 lg:gap-x-11 lg:gap-y-11">
+    // <div className="bg-white w-screen overflow-hidden max-h-screen pt-24 grid grid-cols-2 px-6 md:px-0 md:grid-cols-3 gap-y-3.5 gap-x-4 md:gap-x-8 md:gap-y-8 lg:gap-x-11 lg:gap-y-11">
+    <div className="w-screen max-h-screen overflow-hidden bg-white pt-24 grid grid-cols-2 px-6 md:px-0 md:grid-cols-4 gap-y-3.5 gap-x-4 md:gap-x-8 md:gap-y-8 lg:gap-[42px]">
       <InfiniteVerticalCarousel
         withSlow
         render={(item, index) => (
@@ -47,6 +48,20 @@ export default function Portfolio() {
         )}
         direction="bottomToTop"
         images={images3}
+        fastDuration={55}
+      />
+      <InfiniteVerticalCarousel
+        withSlow
+        render={(item, index) => (
+          <ImageCard
+            className="h-[426px] lg:min-w-[300px]"
+            image={item}
+            key={index}
+            withOverlay
+          />
+        )}
+        direction="topToBottom"
+        images={images1}
         fastDuration={55}
       />
     </div>
