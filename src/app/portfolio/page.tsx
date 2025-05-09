cff718abc34ebe8ffedcde14fce470d485b28a9d
Fenyep <0Uiimi1D5/-yYkGt>
2025-05-09 15:24:51 +0100

    Removed animation on the portfolio page

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,37 +1,37 @@
 "use client";
 
 import ImageCard from "@/components/card/ImageCard";
-import InfiniteCarousel from "@/components/carousel/InfiniteCarousel";
+// import InfiniteCarousel from "@/components/carousel/InfiniteCarousel";
 import { images1, images2, images3 } from "@/lib/data";
 
 export default function Portfolio() {
   return (
     // <div className="bg-white w-screen overflow-hidden max-h-screen pt-24 grid grid-cols-2 px-6 md:px-0 md:grid-cols-3 gap-y-3.5 gap-x-4 md:gap-x-8 md:gap-y-8 lg:gap-x-11 lg:gap-y-11">
-    <div className="w-screen max-h-[150rem] sm:max-h-[175rem] xl:max-h-[225rem] overflow-hidden bg-white pt-24 grid grid-cols-2 px-6 md:px-0 md:grid-cols-3 gap-y-3.5 gap-x-4 md:gap-x-6 lg:gap-x-8 md:gap-y-8 lg:gap-[42px]">
-      <InfiniteCarousel
+    <div className="w-screen bg-white pt-24 grid grid-cols-2 px-6 md:px-0 md:grid-cols-3 gap-y-3.5 gap-x-4 md:gap-x-6 lg:gap-x-8 md:gap-y-8 lg:gap-[42px]">
+      {/* <InfiniteCarousel
         hoverBehavior="pause"
         direction="bottomToTop"
-        fastDuration={75}>
-        {images1.map((item, index) => (
-          <ImageCard className="" image={item} key={index} withOverlay />
-        ))}
-      </InfiniteCarousel>
-      <InfiniteCarousel
+        fastDuration={75}> */}
+      {images1.map((item, index) => (
+        <ImageCard className="" image={item} key={index} withOverlay />
+      ))}
+      {/* </InfiniteCarousel> */}
+      {/* <InfiniteCarousel
         hoverBehavior="pause"
         direction="topToBottom"
-        fastDuration={75}>
-        {images2.map((item, index) => (
-          <ImageCard className="" image={item} key={index} withOverlay />
-        ))}
-      </InfiniteCarousel>
-      <InfiniteCarousel
+        fastDuration={75}> */}
+      {images2.map((item, index) => (
+        <ImageCard className="" image={item} key={index} withOverlay />
+      ))}
+      {/* </InfiniteCarousel> */}
+      {/* <InfiniteCarousel
         hoverBehavior="pause"
         direction="bottomToTop"
-        fastDuration={75}>
-        {images3.map((item, index) => (
-          <ImageCard className="" image={item} key={index} withOverlay />
-        ))}
-      </InfiniteCarousel>
+        fastDuration={75}> */}
+      {images3.map((item, index) => (
+        <ImageCard className="" image={item} key={index} withOverlay />
+      ))}
+      {/* </InfiniteCarousel> */}
     </div>
   );
 }
